a70e5c24637272c6f757f88e3c94afd3c8fff948
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-05-08 11:11:54 -0300

    feat: Add validation for user input in validacaoLista.js
    This commit adds a new file `validacaoLista.js` which contains a function `run` that performs validation on user input for a list. The function checks if the input matches any of the predefined patterns and returns the corresponding list item. This addition enhances the functionality of the application by providing validation for list inputs, improving user interaction and preventing unexpected input errors.

---
+++ validacoes/validacaoLista.js
@@ -0,0 +1,31 @@
+function run(inputUsuario, dynamicContent) {
+  const posicoes = {
+      "1": "primeir[oa]|u[mn]",
+      "2": "segund[ao]|do[ie]s",
+      "3": "terceir[ao]|tr[eê]s",
+      "4": "quart[ao]|[qc]uat(r)?o",
+      "5": "quint[ao]|[sc]inc[ou]",
+      "6": "sext[ao]|[sc]inc[uo]",
+      "7": "s[eé]tim[ao]|[sc]et[ei]",
+      "8": "oitav[ao]|oit[ou]",
+      "9": "non[ao]|nov[ei]",
+      "10": "d[eé][cs]im[ou]|d[ée][sz]"
+  }
+  try {
+      if (!inputUsuario) return 'inatividade'
+      const regexDictionary = {}
+      const { rows } = JSON.parse(dynamicContent).interactive.action.sections[0]
+      rows.forEach((button, index) => {
+          const key = `^(${index + 1}|${posicoes[index + 1]}|${button.title.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()})$`
+          regexDictionary[key] = button.title
+      })
+      for (let regex in regexDictionary) {
+          if (new RegExp(regex, 'i').test(inputUsuario.normalize("NFD").replace(/[\u0300-\u036f]/g, ""))) {
+              return regexDictionary[regex]
+          }
+      }
+      return 'input inesperado'
+  } catch (err) {
+      return 'input inesperado'
+  }
+}
