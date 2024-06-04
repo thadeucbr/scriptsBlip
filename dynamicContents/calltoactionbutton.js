eb99b5b7e1075aea80ec002e84efbd3a0308ea3e
Victor Rocha <xhugo@live.com>
2024-06-04 18:18:48 -0300

    add cta button

---
+++ dynamicContents/calltoactionbutton.js
@@ -0,0 +1,17 @@
+const dynamicContent = {
+    "recipient_type": "individual",
+    "type": "interactive",
+    "interactive": {
+        "type": "cta_url",
+        "body": {
+          "text": "Olá, informamos que a partir de julho, este número será desativado e o seu atendimento será realizado pelo número: (11) 3175-8248.\n\nPara um atendimento ainda mais completo, disponibilizamos este novo número.\nCom ele, você poderá esclarecer dúvidas, fazer consultas, acessar serviços e muito mais.\n\nToque no botão abaixo para continuar."
+        },
+        "action": {
+          "name": "cta_url",
+          "parameters": {
+            "display_text": "Abrir WhatsAp PJ",
+            "url": "https://novotestevalida-vglka.blip.ai/application/detail/exemplos4/templates/builder/"
+          }
+        }
+      }
+}
