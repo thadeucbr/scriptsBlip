0811cb9e50c25f7a24bb7fb9953329effffad52e
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-07-08 15:44:24 -0300

    feat: Add support for abbreviated month names in date extractor
    The code changes in `extratorDeDatas.js` introduce support for abbreviated month names in the date extractor. This allows users to input dates using abbreviations like "jan" for January or "fev" for February. The `months` object has been updated to include mappings for the abbreviated month names.
    
    This commit message follows the established convention of using a prefix to indicate the type of change (`feat` for feature).

--- scripts/dataExtractor/extratorDeDatasV2.js
+++ scripts/dataExtractor/extratorDeDatasV2.js
@@ -1,150 +1,261 @@
+// function run(input) {
+//   const months = {
+//       janeiro: '01', fevereiro: '02', março: '03', abril: '04', maio: '05', junho: '06',
+//       julho: '07', agosto: '08', setembro: '09', outubro: '10', novembro: '11', dezembro: '12',
+//       jan: '01', fev: '02', mar: '03', abr: '04', mai: '05', jun: '06',
+//       jul: '07', ago: '08', set: '09', out: '10', nov: '11', dez: '12'
+//   };
+
+//   const currentDate = new Date();
+//   const currentYear = currentDate.getFullYear();
+//   const ninetyDaysInMillis = 90 * 24 * 60 * 60 * 1000;
+
+//   function parseDate(dateStr) {
+//       let [day, month, year] = dateStr.split('/');
+//       day = day.padStart(2, '0');
+//       month = month.padStart(2, '0');
+//       year = year ? (year.length === 2 ? '20' + year : year) : currentYear;
+//       return new Date(year, month - 1, day);
+//   }
+
+//   function formatDate(date) {
+//       const day = String(date.getDate()).padStart(2, '0');
+//       const month = String(date.getMonth() + 1).padStart(2, '0');
+//       const year = date.getFullYear();
+//       return `${day}/${month}/${year}`;
+//   }
+
+//   function getMonthNumber(monthName) {
+//       return months[monthName.toLowerCase()];
+//   }
+
+//   function extractDates(text) {
+//       const regexPatterns = [
+//           { pattern: /[uú]ltimos (\d{1,2}) dias/i, handler: match => {
+//               const numberOfDays = parseInt(match[1], 10);
+//               const endDate = currentDate;
+//               const startDate = new Date(endDate.getTime() - numberOfDays * 24 * 60 * 60 * 1000);
+//               return [startDate, endDate];
+//           }},
+//           { pattern: /(\d{1,2}) de (\w+) até (\d{1,2}) de (\w+)/i, handler: match => {
+//               const startDay = match[1].padStart(2, '0');
+//               const endDay = match[3].padStart(2, '0');
+//               const startMonth = getMonthNumber(match[2]);
+//               const endMonth = getMonthNumber(match[4]);
+//               const year = currentYear;
+//               return [parseDate(`${startDay}/${startMonth}/${year}`), parseDate(`${endDay}/${endMonth}/${year}`)];
+//           }},
+//           { pattern: /de (\w+) até (\w+)/i, handler: match => {
+//               const startMonth = getMonthNumber(match[1]);
+//               const endMonth = getMonthNumber(match[2]);
+//               const year = currentYear;
+//               return [parseDate(`01/${startMonth}/${year}`), new Date(year, parseInt(endMonth, 10), 0)];
+//           }},
+//           { pattern: /de (\w+)/i, handler: match => {
+//               const month = getMonthNumber(match[1]);
+//               const year = currentYear;
+//               return [parseDate(`01/${month}/${year}`), new Date(year, parseInt(month, 10), 0)];
+//           }},
+//           { pattern: /(\d{1,2}\/\d{1,2}\/\d{2,4})/g, handler: match => match.map(date => parseDate(date))},
+//           { pattern: /(\d{1,2}\/\d{1,2})/g, handler: match => match.map(dateStr => parseDate(`${dateStr}/${currentYear}`))},
+//           { pattern: /(\d{1,2}\/\d{1,2}) até (\d{1,2}\/\d{1,2})/i, handler: match => {
+//               const year = currentYear;
+//               return [parseDate(`${match[1]}/${year}`), parseDate(`${match[2]}/${year}`)];
+//           }},
+//           { pattern: /dia (\d{1,2})/i, handler: match => {
+//               const day = match[1].padStart(2, '0');
+//               const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+//               return [parseDate(`${day}/${month}/${currentYear}`)];
+//           }},
+//           { pattern: /([úu]ltimo|mês) passad(o)/i, handler: () => {
+//               const lastMonthStartDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
+//               const lastMonthEndDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
+//               return [lastMonthStartDate, lastMonthEndDate];
+//           }},
+//           { pattern: /([úu]ltima semana|semana passada)/i, handler: () => {
+//               const lastWeekEndDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - currentDate.getDay());
+//               const lastWeekStartDate = new Date(lastWeekEndDate.getFullYear(), lastWeekEndDate.getMonth(), lastWeekEndDate.getDate() - 6);
+//               return [lastWeekStartDate, lastWeekEndDate];
+//           }},
+//           { pattern: /deste m[eê]s/i, handler: () => {
+//               const thisMonthStartDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+//               const thisMonthEndDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+//               return [thisMonthStartDate, thisMonthEndDate];
+//           }},
+//           { pattern: /([úu]ltim(a|o)|trimestre) (passad(a|o)|trimestre)/i, handler: () => {
+//               const currentQuarter = Math.floor(currentDate.getMonth() / 3) + 1;
+//               let lastQuarter, lastQuarterYear;
+//               if (currentQuarter === 1) {
+//                   lastQuarter = 4;
+//                   lastQuarterYear = currentYear - 1;
+//               } else {
+//                   lastQuarter = currentQuarter - 1;
+//                   lastQuarterYear = currentYear;
+//               }
+//               const startMonth = (lastQuarter - 1) * 3;
+//               const endMonth = startMonth + 2;
+//               return [new Date(lastQuarterYear, startMonth, 1), new Date(lastQuarterYear, endMonth + 1, 0)];
+//           }},
+//           { pattern: /([úu]ltim(a|o)|bimestre) (passad(a|o)|bimestre)/i, handler: () => {
+//               const currentBimester = Math.floor(currentDate.getMonth() / 2) + 1;
+//               let lastBimester, lastBimesterYear;
+//               if (currentBimester === 1) {
+//                   lastBimester = 6;
+//                   lastBimesterYear = currentYear - 1;
+//               } else {
+//                   lastBimester = currentBimester - 1;
+//                   lastBimesterYear = currentYear;
+//               }
+//               const startMonth = (lastBimester - 1) * 2;
+//               const endMonth = startMonth + 1;
+//               return [new Date(lastBimesterYear, startMonth, 1), new Date(lastBimesterYear, endMonth + 1, 0)];
+//           }}
+//       ];
+
+//       for (const { pattern, handler } of regexPatterns) {
+//           const match = text.match(pattern);
+//           if (match) {
+//               return handler(match);
+//           }
+//       }
+
+//       return [];
+//   }
+
+//   const dates = extractDates(input);
+
+//   if (dates.length === 1) {
+//       const date = dates[0];
+//       if (date > currentDate || (currentDate - date) > ninetyDaysInMillis) {
+//           return 'Data não pode ser superior à data atual e deve estar dentro do intervalo de 90 dias';
+//       }
+//       return formatDate(date);
+//   } else if (dates.length === 2) {
+//       const [startDate, endDate] = dates;
+//       if (startDate > currentDate || endDate > currentDate) {
+//           return 'Datas não podem ser superiores à data atual';
+//       }
+//       const diffInMillis = endDate - startDate;
+//       if (diffInMillis > 0 && diffInMillis <= ninetyDaysInMillis) {
+//           return `${formatDate(startDate)} - ${formatDate(endDate)}`;
+//       } else {
+//           return 'Período deve ser inferior a 90 dias';
+//       }
+//   } else {
+//       return 'Formato de data não reconhecido';
+//   }
+// }
+
 function run(input) {
-  const months = {
-      janeiro: '01', fevereiro: '02', março: '03', abril: '04', maio: '05', junho: '06',
-      julho: '07', agosto: '08', setembro: '09', outubro: '10', novembro: '11', dezembro: '12',
-      jan: '01', fev: '02', mar: '03', abr: '04', mai: '05', jun: '06',
-      jul: '07', ago: '08', set: '09', out: '10', nov: '11', dez: '12'
-  };
+    try {
+        const months = {
+            janeiro: '01', fevereiro: '02', março: '03', abril: '04', maio: '05', junho: '06',
+            julho: '07', agosto: '08', setembro: '09', outubro: '10', novembro: '11', dezembro: '12',
+            jan: '01', fev: '02', mar: '03', abr: '04', mai: '05', jun: '06',
+            jul: '07', ago: '08', set: '09', out: '10', nov: '11', dez: '12'
+        };
 
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const ninetyDaysInMillis = 90 * 24 * 60 * 60 * 1000;
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const ninetyDaysInMillis = 90 * 24 * 60 * 60 * 1000;
 
-  function parseDate(dateStr) {
-      let [day, month, year] = dateStr.split('/');
-      day = day.padStart(2, '0');
-      month = month.padStart(2, '0');
-      year = year ? (year.length === 2 ? '20' + year : year) : currentYear;
-      return new Date(year, month - 1, day);
-  }
+        function parseDate(dateStr) {
+            const parts = dateStr.split('/');
+            const day = parts[0].padStart(2, '0');
+            const month = parts[1].padStart(2, '0');
+            const year = parts[2] ? (parts[2].length === 2 ? '20' + parts[2] : parts[2]) : currentYear;
+            return new Date(year, month - 1, day);
+        }
 
-  function formatDate(date) {
-      const day = String(date.getDate()).padStart(2, '0');
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const year = date.getFullYear();
-      return `${day}/${month}/${year}`;
-  }
+        function formatDate(date) {
+            const day = String(date.getDate()).padStart(2, '0');
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const year = date.getFullYear();
+            return `${day}/${month}/${year}`;
+        }
 
-  function getMonthNumber(monthName) {
-      return months[monthName.toLowerCase()];
-  }
+        function getMonthNumber(monthName) {
+            return months[monthName.toLowerCase()];
+        }
 
-  function extractDates(text) {
-      const regexPatterns = [
-          { pattern: /[uú]ltimos (\d{1,2}) dias/i, handler: match => {
-              const numberOfDays = parseInt(match[1], 10);
-              const endDate = currentDate;
-              const startDate = new Date(endDate.getTime() - numberOfDays * 24 * 60 * 60 * 1000);
-              return [startDate, endDate];
-          }},
-          { pattern: /(\d{1,2}) de (\w+) até (\d{1,2}) de (\w+)/i, handler: match => {
-              const startDay = match[1].padStart(2, '0');
-              const endDay = match[3].padStart(2, '0');
-              const startMonth = getMonthNumber(match[2]);
-              const endMonth = getMonthNumber(match[4]);
-              const year = currentYear;
-              return [parseDate(`${startDay}/${startMonth}/${year}`), parseDate(`${endDay}/${endMonth}/${year}`)];
-          }},
-          { pattern: /de (\w+) até (\w+)/i, handler: match => {
-              const startMonth = getMonthNumber(match[1]);
-              const endMonth = getMonthNumber(match[2]);
-              const year = currentYear;
-              return [parseDate(`01/${startMonth}/${year}`), new Date(year, parseInt(endMonth, 10), 0)];
-          }},
-          { pattern: /de (\w+)/i, handler: match => {
-              const month = getMonthNumber(match[1]);
-              const year = currentYear;
-              return [parseDate(`01/${month}/${year}`), new Date(year, parseInt(month, 10), 0)];
-          }},
-          { pattern: /(\d{1,2}\/\d{1,2}\/\d{2,4})/g, handler: match => match.map(date => parseDate(date))},
-          { pattern: /(\d{1,2}\/\d{1,2})/g, handler: match => match.map(dateStr => parseDate(`${dateStr}/${currentYear}`))},
-          { pattern: /(\d{1,2}\/\d{1,2}) até (\d{1,2}\/\d{1,2})/i, handler: match => {
-              const year = currentYear;
-              return [parseDate(`${match[1]}/${year}`), parseDate(`${match[2]}/${year}`)];
-          }},
-          { pattern: /dia (\d{1,2})/i, handler: match => {
-              const day = match[1].padStart(2, '0');
-              const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-              return [parseDate(`${day}/${month}/${currentYear}`)];
-          }},
-          { pattern: /([úu]ltimo|mês) passad(o)/i, handler: () => {
-              const lastMonthStartDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
-              const lastMonthEndDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
-              return [lastMonthStartDate, lastMonthEndDate];
-          }},
-          { pattern: /([úu]ltima semana|semana passada)/i, handler: () => {
-              const lastWeekEndDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - currentDate.getDay());
-              const lastWeekStartDate = new Date(lastWeekEndDate.getFullYear(), lastWeekEndDate.getMonth(), lastWeekEndDate.getDate() - 6);
-              return [lastWeekStartDate, lastWeekEndDate];
-          }},
-          { pattern: /deste m[eê]s/i, handler: () => {
-              const thisMonthStartDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-              const thisMonthEndDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
-              return [thisMonthStartDate, thisMonthEndDate];
-          }},
-          { pattern: /([úu]ltim(a|o)|trimestre) (passad(a|o)|trimestre)/i, handler: () => {
-              const currentQuarter = Math.floor(currentDate.getMonth() / 3) + 1;
-              let lastQuarter, lastQuarterYear;
-              if (currentQuarter === 1) {
-                  lastQuarter = 4;
-                  lastQuarterYear = currentYear - 1;
-              } else {
-                  lastQuarter = currentQuarter - 1;
-                  lastQuarterYear = currentYear;
-              }
-              const startMonth = (lastQuarter - 1) * 3;
-              const endMonth = startMonth + 2;
-              return [new Date(lastQuarterYear, startMonth, 1), new Date(lastQuarterYear, endMonth + 1, 0)];
-          }},
-          { pattern: /([úu]ltim(a|o)|bimestre) (passad(a|o)|bimestre)/i, handler: () => {
-              const currentBimester = Math.floor(currentDate.getMonth() / 2) + 1;
-              let lastBimester, lastBimesterYear;
-              if (currentBimester === 1) {
-                  lastBimester = 6;
-                  lastBimesterYear = currentYear - 1;
-              } else {
-                  lastBimester = currentBimester - 1;
-                  lastBimesterYear = currentYear;
-              }
-              const startMonth = (lastBimester - 1) * 2;
-              const endMonth = startMonth + 1;
-              return [new Date(lastBimesterYear, startMonth, 1), new Date(lastBimesterYear, endMonth + 1, 0)];
-          }}
-      ];
+        function extractDates(text) {
+            const patterns = [
+                { pattern: /[uú]ltimos (\d{1,2}) dias/i, handler: match => {
+                    const numberOfDays = parseInt(match[1], 10);
+                    const endDate = currentDate;
+                    const startDate = new Date(endDate.getTime() - numberOfDays * 24 * 60 * 60 * 1000);
+                    return [startDate, endDate];
+                }},
+                { pattern: /(\d{1,2}) de (\w+) até (\d{1,2}) de (\w+)/i, handler: match => {
+                    const startDay = match[1].padStart(2, '0');
+                    const endDay = match[3].padStart(2, '0');
+                    const startMonth = getMonthNumber(match[2]);
+                    const endMonth = getMonthNumber(match[4]);
+                    const year = currentYear;
+                    return [parseDate(`${startDay}/${startMonth}/${year}`), parseDate(`${endDay}/${endMonth}/${year}`)];
+                }},
+                { pattern: /(\d{1,2}\/\d{1,2}\/\d{2,4})/g, handler: match => match.map(date => parseDate(date))},
+                { pattern: /(\d{1,2}\/\d{1,2}) até (\d{1,2}\/\d{1,2})/i, handler: match => {
+                    const year = currentYear;
+                    return [parseDate(`${match[1]}/${year}`), parseDate(`${match[2]}/${year}`)];
+                }},
+                { pattern: /(\d{1,2}\/\d{1,2})/g, handler: match => match.map(dateStr => parseDate(`${dateStr}/${currentYear}`))},
+                { pattern: /de (\w+) até (\w+)/i, handler: match => {
+                    const startMonth = getMonthNumber(match[1]);
+                    const endMonth = getMonthNumber(match[2]);
+                    const year = currentYear;
+                    return [parseDate(`01/${startMonth}/${year}`), new Date(year, parseInt(endMonth, 10), 0)];
+                }},
+                { pattern: /de (\w+)/i, handler: match => {
+                    const month = getMonthNumber(match[1]);
+                    const year = currentYear;
+                    return [parseDate(`01/${month}/${year}`), new Date(year, parseInt(month, 10), 0)];
+                }},
+                { pattern: /dia (\d{1,2})/i, handler: match => {
+                    const day = match[1].padStart(2, '0');
+                    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+                    return [parseDate(`${day}/${month}/${currentYear}`)];
+                }}
+            ];
 
-      for (const { pattern, handler } of regexPatterns) {
-          const match = text.match(pattern);
-          if (match) {
-              return handler(match);
-          }
-      }
+            for (const { pattern, handler } of patterns) {
+                const match = text.match(pattern);
+                if (match) {
+                    return handler(match);
+                }
+            }
 
-      return [];
-  }
+            return [];
+        }
 
-  const dates = extractDates(input);
+        const dates = extractDates(input);
 
-  if (dates.length === 1) {
-      const date = dates[0];
-      if (date > currentDate || (currentDate - date) > ninetyDaysInMillis) {
-          return 'Data não pode ser superior à data atual e deve estar dentro do intervalo de 90 dias';
-      }
-      return formatDate(date);
-  } else if (dates.length === 2) {
-      const [startDate, endDate] = dates;
-      if (startDate > currentDate || endDate > currentDate) {
-          return 'Datas não podem ser superiores à data atual';
-      }
-      const diffInMillis = endDate - startDate;
-      if (diffInMillis > 0 && diffInMillis <= ninetyDaysInMillis) {
-          return `${formatDate(startDate)} - ${formatDate(endDate)}`;
-      } else {
-          return 'Período deve ser inferior a 90 dias';
-      }
-  } else {
-      return 'Formato de data não reconhecido';
-  }
+        if (dates.length === 1) {
+            const date = dates[0];
+            if (date > currentDate || (currentDate - date) > ninetyDaysInMillis) {
+                return 'Data não pode ser superior à data atual e deve estar dentro do intervalo de 90 dias';
+            }
+            return formatDate(date);
+        } else if (dates.length === 2) {
+            const [startDate, endDate] = dates;
+            if (startDate > currentDate || endDate > currentDate) {
+                return 'Datas não podem ser superiores à data atual';
+            }
+            const diffInMillis = endDate - startDate;
+            if (diffInMillis > 0 && diffInMillis <= ninetyDaysInMillis) {
+                return `${formatDate(startDate)} - ${formatDate(endDate)}`;
+            } else {
+                return 'Período deve ser inferior a 90 dias';
+            }
+        } else {
+            return 'Formato de data não reconhecido';
+        }
+    } catch (err) {
+        return 'Erro ao processar as datas: ' + err.message;
+    }
 }
+
+
 
 // const testCases = [
 //     "Preciso do extrato dos últimos 30 dias",
