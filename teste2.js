635a75ff8718c46d3c8d49ada587aa125ae6254f
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2024-07-19 11:35:26 -0300

    backup

---
+++ teste2.js
@@ -0,0 +1,37 @@
+[
+  {
+    id: '1',
+    title: 'Perda ou Roubo',
+    description: 'Bloqueio por perda ou roubo'
+  },
+  {
+    id: '2',
+    title: 'Máquinas Safrapay',
+    description: 'Condições e taxas, Suporte, Vendas, Antecipação, entre outros.'
+  },
+  {
+    id: '3',
+    title: 'Conta Corrente',
+    description: 'Pix, Falar sobre conta, Consultar Pacotes'
+  },
+  {
+    id: '4',
+    title: 'Soluções',
+    description: 'Consulta de protocolo, Boletos, Descredenciamento'
+  },
+  {
+    id: '5',
+    title: 'Apoio à Navegação',
+    description: 'Acesso, Alterações cadastrais, Alterar domicílio'
+  },
+  {
+    id: '6',
+    title: 'Transação não autorizada',
+    description: 'Meu pagamento não foi autorizado'
+  },
+  {
+    id: '7',
+    title: 'Produtos',
+    description: 'Seguros, Investimento, Renegociação, Gravame, Desempenho e Cartão'
+  }
+]
