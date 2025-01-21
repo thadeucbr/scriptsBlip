8faf10ca98ec0aaa20bab65ce1a8728ce5376517
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2025-01-21 14:53:29 -0300

    atualiza função updateEnv para suportar projetos específicos nos ambientes de origem e destino

--- scripts/syncEnv/src/updateEnv.js
+++ scripts/syncEnv/src/updateEnv.js
@@ -1,21 +1,25 @@
-import decodeName from './decodeName.js'
-import getBotJsonAndUpdateFlow from './getBotJsonAndUpdateFlow.js'
-import vars from '../../shared/vars.js'
-async function updateEnv(origin, destination) {
-  const keyOrigin = vars[origin].map(({key, name}) => ({key, name}))
-  const keyDestination = vars[destination].map(({key, name}) => ({key, name}))
+import decodeName from './decodeName.js';
+import getBotJsonAndUpdateFlow from './getBotJsonAndUpdateFlow.js';
+import vars from '../../shared/vars.js';
+
+async function updateEnv(origin, destination, project) {
+  // Acessa os bots do projeto específico no ambiente de origem e destino
+  const keyOrigin = vars[origin][project] || []; // Pega os bots do projeto ou um array vazio se não existir
+  const keyDestination = vars[destination][project] || []; // Pega os bots do projeto ou um array vazio se não existir
+  
   for (const iterator of keyOrigin) {
-    const {key, name} = iterator
-    const destinationKey = keyDestination.find(({name: nameDestination}) => name === nameDestination)
+    const { key, name } = iterator;
+    const destinationKey = keyDestination.find(({ name: nameDestination }) => name === nameDestination);
     if (destinationKey) {
-      decodeName(key, destinationKey.key)
-      await delay(1000)
-      getBotJsonAndUpdateFlow(key, destinationKey.key)
+      decodeName(key, destinationKey.key);
+      await delay(1000);
+      getBotJsonAndUpdateFlow(key, destinationKey.key);
     }
   }
 }
 
 function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
-export default updateEnv
+
+export default updateEnv;
